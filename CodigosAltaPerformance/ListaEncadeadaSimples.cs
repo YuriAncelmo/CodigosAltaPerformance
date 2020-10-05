113316119bff0181f8200d2a355ff7957b81e20a
YuriAncelmo <yuriancelmo@hotmail.com>
2020-10-05 08:45:07 -0300

    Initial commit

---
+++ CodigosAltaPerformance/ListaEncadeadaSimples.cs
@@ -0,0 +1,69 @@
+﻿
+using System;
+
+namespace CodigosAltaPerformance
+{
+    class ListaEncadeadaSimples
+    {
+        public Node lista { get; set; }
+        public class Node
+        {
+            public int dado;
+            public Node prox;
+        }
+        /// <summary>
+        /// Cria uma instancia de lista encadeada simples
+        /// </summary>
+        /// <param name="numero_elementos">Representa a quantidade de nós que serão gerados</param>
+        public ListaEncadeadaSimples(int numero_elementos)
+        {
+            InicializarLista(numero_elementos);
+        }
+
+        private void InicializarLista(int numero_elementos)
+        {
+            //Inicia a lista com ponteiro null
+            lista = null;
+
+            for (int i = numero_elementos; i > 0; i--)
+            {
+                Node novo = new Node();
+                novo.dado = i * 2 + 5;
+                //Na primeira execução é nulo
+                //Na segunda execução é o úl/timo elemento
+                novo.prox = lista;
+                //primeiro elemento da fila 
+                lista = novo;
+            }
+        }
+
+        public void MostrarDados()
+        {
+            MostrarDados(lista);
+        }
+        private void MostrarDados(Node lista)
+        {
+            Node aux = lista;
+            Console.WriteLine("Dado" + aux.dado);
+            if(aux.prox != null) MostrarDados(aux.prox);
+        }
+        public void InserirElemento(int valor)
+        {
+            Node aux = lista;
+
+            while(aux.prox != null)
+            {
+                if (aux.dado < valor && aux.prox.dado > valor)
+                {
+                    Node novo = new Node();
+                    novo.dado = valor;
+                    novo.prox = aux.prox;
+                    aux.prox = novo;
+                    break;
+                }
+                else aux = aux.prox;
+            }
+            //lista = aux; Não é necessário pois a lista já contem o primeiro elemento 
+        }
+    }
+}
---
+++ CodigosAltaPerformance/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodigosAltaPerformance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ListaEncadeadaSimples simples = new ListaEncadeadaSimples(100);
+            simples.MostrarDados();
+            
+            simples.InserirElemento(8);
+
+            Console.WriteLine("Lista depois da inserção ");
+            simples.MostrarDados();
+            
+            Console.ReadKey();
+        }
+    }
+}
